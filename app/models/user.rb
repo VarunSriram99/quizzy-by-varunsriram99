fc407d6783c86dbbc1012e52f1dec03d604fc9b2
S Varun <s.varun@bigbinary.com>
2021-11-02 22:25:32 +0530

    Added has secure password, bcrypt and password digest column

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,21 @@
 
 class User < ApplicationRecord
   enum role: { standard: 0, administrator: 1 }
+
+  has_secure_password
+
   validates :role, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false },
 format: { with: Constants::EMAIL_VALIDATION_REGEX }
   validates :first_name, presence: true, length: { maximum: 50 }
   validates :last_name, presence: true, length: { maximum: 50 }
-  before_create :convert_email_to_lowercase
+  validates :password, presence: true, length: { minimum: 7 }
+  validates :password_confirmation, presence: true, on: :create
+  before_save :convert_email_to_lowercase
 
   private
 
     def convert_email_to_lowercase
-      self.email = self.email.downcase
+      email.downcase!
     end
 end
